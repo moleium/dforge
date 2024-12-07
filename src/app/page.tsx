a4b2c1e83dd028ec388913f628985ba4ed12f2a9
moleium <molenoch@protonmail.com>
2024-12-07 20:29:21 +0300

    fix: allow importing with ts extension

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { DargPreviewer } from '@/previewer/Previewer'
 import { useState } from 'react'
-import { DargEditor } from '@/components/editor'
+import { DargEditor } from '@/components/editor.tsx' // weird
 
 export default function Page() {
   const [squirrelCode, setSquirrelCode] = useState(`
