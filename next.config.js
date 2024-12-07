30f481bab4c267d4e63ca1addc53336136db9b3c
moleium <molenoch@protonmail.com>
2024-12-07 20:21:20 +0300

    fix: remove experimental config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   transpilePackages: ['highlight.js'],
   webpack: (config) => {
     config.resolve.alias = {
